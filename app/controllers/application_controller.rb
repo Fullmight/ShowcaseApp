31fef4fa3fe2a64c0f39a455a104587f6b97dd85
Cameron Pick <yakri13@gmail.com>
2017-10-14 11:20:53 -0700

    Added rails generated pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def intro
-    render html: "<h1>Welcome to the cyberscape.</h1>"
+    render html: '<h1>Welcome to the cyberscape.</h1>'
   end
 end
