9ecd71ad304c391b0323a53a156fe4038ff2ca93
Cameron Pick <yakri13@gmail.com>
2017-10-20 15:30:26 -0700

    Added contact page and tests; updates to home and about sections.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,34 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Lorem Ipsum" #example of variable use in test
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "h1", "#{@base_title}" #Placeholder; when this was written
+                                         #All tests were different
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "h1", "lorem ipsum"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "h1", "About"
+  end
+
+  test "should get contact" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "h1", "Contact"
   end
 
 end
